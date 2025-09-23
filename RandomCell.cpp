c04d2acc5e98f95f186c2181f94900a1d38ef3d9
Федорчук Христина <fedorcukhristina25@gmail.com>
2025-09-23 10:19:52 +0300

    my first commit

--- RandomCell.cpp
+++ RandomCell.cpp
@@ -1,31 +1,25 @@
-#include <stdexcept>
-#include <chrono>
 #include "RandomCell.h"
-
+#include <stdexcept>
+#include <random> 
 
 RandomCell::RandomCell(int n)
-	: n_(n),
-	rng_(static_cast<std::uint32_t>(std::chrono::steady_clock::now().time_since_epoch().count() ^ std::random_device{}())),
-	dist_(0, n - 1)
+    : n_(n), dist_(0, n - 1)
 {
-	if (n <= 0) {
-		throw std::invalid_argument("RandomCell: board size n must be > 0");
-	}
+    if (n <= 0) {
+        throw std::invalid_argument("board size must be > 0");
+    }
+    std::random_device rd;
+    rng_.seed(rd());
 }
-
 
 RandomCell::RandomCell(int n, std::uint32_t seed)
-	: n_(n), rng_(seed), dist_(0, n - 1)
+    : n_(n), rng_(seed), dist_(0, n - 1)
 {
-	if (n <= 0) {
-		throw std::invalid_argument("RandomCell: board size n must be > 0");
-	}
+    if (n <= 0) {
+        throw std::invalid_argument("board size must be > 0");
+    }
 }
 
-
 std::pair<int, int> RandomCell::operator()() {
-	int flat = dist_(rng_) * n_ + dist_(rng_); 
-	int row = dist_(rng_);
-	int col = dist_(rng_);
-	return { row, col };
+    return { dist_(rng_), dist_(rng_) };
 }
